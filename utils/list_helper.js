6449dd9276b5c865cf30c54cf28924ddb7ec1479
hamesomer11 <hamzaomer45@gmail.com>
2024-08-07 10:10:06 +0100

    Helper Functions and Unit Tests: Exercise 4.4 - commit #1

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,20 @@
 const dummy = (blogs) => {
     return 1;
 }
+
+const totalLikes = (array) => {
+
+    if(array.length === 0) {
+        return 0
+    }
+
+    for(const i in array) {
+        return array[i].likes;
+    }
+
+}
   
 module.exports = {
-   dummy
+   dummy,
+   totalLikes
 }
