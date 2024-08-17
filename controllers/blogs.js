92bd5a18f1f6453106007ebd59cb9925df0f0c98
hamesomer11 <hamzaomer45@gmail.com>
2024-08-17 14:39:39 +0100

    User/Token administration: Exercise 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,10 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 require('express-async-errors')
 
 blogsRouter.get('/', async (request, response) => { 
-  const notes = await Blog.find({})
-  response.json(notes)
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
 })
   
 blogsRouter.get('/:id', (request, response, next) => {
@@ -23,17 +25,22 @@ blogsRouter.get('/:id', (request, response, next) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user.id
   })
   
   if(blog.title === "" || blog.url === "") {
     response.status(400).json()
   } else {
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog)
   }
 })
